6f456d814a3208564f4fdbf509a1d0a6a4b886a2
sunghun <nokdonwithkj211@gmail.com>
2023-11-29 19:13:33 +0900

    feat(Retry, RetryRequest): 재시작 여부를 확인하는 값을 관리하는 클래스 생성

--- src/main/java/bridge/request/Retry.java
+++ src/main/java/bridge/request/Retry.java
@@ -1,2 +1,25 @@
-package bridge.request;public class Retry {
+package bridge.request;
+
+public class Retry {
+    private final String retryValue;
+
+    private Retry(String retryValue) {
+        validateRetryValue(retryValue);
+        this.retryValue = retryValue;
+    }
+
+    public static Retry from(String retryValue) {
+        return new Retry(retryValue);
+    }
+
+    private void validateRetryValue(String retryValue) {
+        isValidWord(retryValue);
+    }
+
+    private void isValidWord(String retryValue) {
+        if (retryValue.equals("Q") || retryValue.equals("R")) {
+            return;
+        }
+        throw new IllegalArgumentException("Q나 R만 입력이 가능합니다.");
+    }
 }
--- src/main/java/bridge/request/RetryRequest.java
+++ src/main/java/bridge/request/RetryRequest.java
@@ -1,2 +1,32 @@
-package bridge.request;public class RetryRequest {
+package bridge.request;
+
+import bridge.parser.PatternRegex;
+
+public class RetryRequest {
+    private final String retryRequest;
+
+    public RetryRequest(String retryRequest) {
+        validateRetryRequest(retryRequest);
+        this.retryRequest = retryRequest;
+    }
+
+    private void validateRetryRequest(String retryRequest) {
+        isNullOrEmpty(retryRequest);
+        hasWordPattern(retryRequest);
+    }
+
+    private void isNullOrEmpty(String retryRequest) {
+        if (retryRequest == null || retryRequest.isEmpty()) {
+            throw new IllegalArgumentException("아무것도 입력하지 않았습니다.");
+        }
+    }
+
+    private void hasWordPattern(String retryRequest) {
+        if (PatternRegex.WORD_PATTERN.matcher(retryRequest).matches()) {
+            return;
+        }
+        throw new IllegalArgumentException("문자를 입력해주세요.");
+    }
+
+
 }
