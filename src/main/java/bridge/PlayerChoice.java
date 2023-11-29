783d7172e910467dddfdccd6bc6b2d500acce1a2
sunghun <nokdonwithkj211@gmail.com>
2023-11-29 15:55:49 +0900

    feat(PlayerChoice): digit, valeu를 관리하는 클래스 생성 및 테스트 작성

---
+++ src/main/java/bridge/PlayerChoice.java
@@ -0,0 +1,23 @@
+package bridge;
+
+public class PlayerChoice {
+    private final String upOrDown;
+    private int digit;
+
+    private PlayerChoice(String upOrDown, int digit) {
+        this.upOrDown = upOrDown;
+        this.digit = digit;
+    }
+
+    public static PlayerChoice of(String upOrDown, int digit) {
+        return new PlayerChoice(upOrDown, digit);
+    }
+
+    public String getUpOrDown() {
+        return upOrDown;
+    }
+
+    public int getDigit() {
+        return digit;
+    }
+}
---
+++ src/test/java/bridge/PlayerChoiceTest.java
@@ -0,0 +1,23 @@
+package bridge;
+
+import java.util.List;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class PlayerChoiceTest {
+    @Test
+    void flow() {
+        BridgeMaker bridgeMaker = new BridgeMaker(new BridgeNumberGenerator() {
+            @Override
+            public int generate() {
+                return 0;
+            }
+        });
+
+        List<String> bridge = bridgeMaker.makeBridge(3);
+        PlayerChoice playerChoice = PlayerChoice.of( "D",0);
+
+        Boolean result = bridge.get(playerChoice.getDigit()) == playerChoice.getUpOrDown();
+        Assertions.assertThat(result).isEqualTo(true);
+    }
+}
