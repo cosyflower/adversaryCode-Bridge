a47d16441795a5dbf61aff09a11d4d238e09e026
sunghun <nokdonwithkj211@gmail.com>
2023-11-29 19:07:33 +0900

    test(MovingDirectionRequest): U나 D를 입력하지 않은 경우에 대한 검증 관련 테스트 작성

---
+++ src/test/java/bridge/request/MovingDirectionRequestTest.java
@@ -0,0 +1,53 @@
+package bridge.request;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Test;
+
+public class MovingDirectionRequestTest {
+    @Nested
+    class 성공 {
+
+        @Nested
+        class MovingDirectionRequest_Test {
+            @Test
+            void flow() {
+                Assertions.assertThatCode(() -> new MovingDirectionRequest("U"))
+                        .doesNotThrowAnyException();
+            }
+        }
+
+        @Nested
+        class MovingDirection_Test {
+            @Test
+            void flow() {
+                Assertions.assertThatCode(() -> new MovingDirectionRequest("U").toMovingDirection())
+                        .doesNotThrowAnyException();
+            }
+        }
+
+    }
+
+    @Nested
+    class 예외 {
+        @Nested
+        class MovingDirectionRequest_Test {
+            @Test
+            void flow() {
+                Assertions.assertThatCode(() -> new MovingDirectionRequest(""))
+                        .isInstanceOf(IllegalArgumentException.class)
+                        .hasMessageContaining("아무것도");
+            }
+        }
+
+        @Nested
+        class MovingDirection_Test {
+            @Test
+            void flow() {
+                Assertions.assertThatCode(() -> new MovingDirectionRequest("A").toMovingDirection())
+                        .isInstanceOf(IllegalArgumentException.class)
+                        .hasMessageContaining("U나 D를");
+            }
+        }
+    }
+}
