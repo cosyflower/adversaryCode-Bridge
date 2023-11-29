a6dd71f7d1b1a821477845122358bc63ba55e173
sunghun <nokdonwithkj211@gmail.com>
2023-11-29 19:01:18 +0900

    feat(MovingDirection, MovingDirectionRequest): 움직일 방향을 관리하는 클래스 생성

--- src/main/java/bridge/request/MovingDirection.java
+++ src/main/java/bridge/request/MovingDirection.java
@@ -1,2 +1,25 @@
-package bridge.request;public class MovingDirection {
+package bridge.request;
+
+public class MovingDirection {
+    private final String movingDirectionValue;
+
+    private MovingDirection(String movingDirectionValue) {
+        validateMovingDirectionValue(movingDirectionValue);
+        this.movingDirectionValue = movingDirectionValue;
+    }
+
+    public static MovingDirection from(String movingDirectionValue) {
+        return new MovingDirection(movingDirectionValue);
+    }
+
+    private void validateMovingDirectionValue(String movingDirectionValue) {
+        isValidWord(movingDirectionValue);
+    }
+
+    private void isValidWord(String movingDirectionValue) {
+        if (movingDirectionValue.equals("U") || movingDirectionValue.equals("D")) {
+            return;
+        }
+        throw new IllegalArgumentException("U나 D를 입력해주세요.");
+    }
 }
--- src/main/java/bridge/request/MovingDirectionRequest.java
+++ src/main/java/bridge/request/MovingDirectionRequest.java
@@ -1,2 +1,35 @@
-package bridge.request;public class MovingDirectionRequest {
+package bridge.request;
+
+import bridge.parser.PatternRegex;
+
+public class MovingDirectionRequest {
+    private final String movingDirectionRequest;
+
+    public MovingDirectionRequest(String movingDirectionRequest) {
+        validateMovingDirectionRequest(movingDirectionRequest);
+        this.movingDirectionRequest = movingDirectionRequest;
+    }
+
+    public MovingDirection toMovingDirection() {
+        return MovingDirection.from(movingDirectionRequest);
+    }
+
+    private void validateMovingDirectionRequest(String movingDirectionRequest) {
+        isNullOrEmpty(movingDirectionRequest);
+        hasWordPattern(movingDirectionRequest);
+    }
+
+    private void isNullOrEmpty(String movingDirectionRequest) {
+        if (movingDirectionRequest == null || movingDirectionRequest.isEmpty()) {
+            throw new IllegalArgumentException("아무것도 입력하지 않았습니다.");
+        }
+    }
+
+    private void hasWordPattern(String movingDirectionRequest) {
+        if (PatternRegex.WORD_PATTERN.matcher(movingDirectionRequest).matches()) {
+            return;
+        }
+        throw new IllegalArgumentException("문자를 입력하지 않았습니다.");
+    }
+
 }
