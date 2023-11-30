3a272149647335ff81b8a2c59b3ee7cdcfa480b5
sunghun <nokdonwithkj211@gmail.com>
2023-11-30 09:09:03 +0900

    feat(PlayerChoice): 이동할 칸을 관리하는 클래스 생성

--- src/main/java/bridge/PlayerChoice.java
+++ src/main/java/bridge/PlayerChoice.java
@@ -1,2 +1,17 @@
-package bridge;public class PlayerChoice {
+package bridge;
+
+public class PlayerChoice {
+    private final String playerChoiceValue;
+
+    public PlayerChoice(String playerChoiceValue) {
+        this.playerChoiceValue = playerChoiceValue;
+    }
+
+    public String getPlayerChoiceValue() {
+        return playerChoiceValue;
+    }
+
+    public boolean hasSameValue(String otherPlayerChoiceValue) {
+        return playerChoiceValue.equals(otherPlayerChoiceValue);
+    }
 }
