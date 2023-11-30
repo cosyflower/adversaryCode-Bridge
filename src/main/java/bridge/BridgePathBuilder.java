dc56e736e7674eb797ce98ece3189b048e552c68
sunghun <nokdonwithkj211@gmail.com>
2023-11-30 09:13:01 +0900

    feat(BridgePathBuilder): 지나온 경로를 Path로 만드는 클래스 생성 및 테스트 코드 작성

--- src/main/java/bridge/BridgePathBuilder.java
+++ src/main/java/bridge/BridgePathBuilder.java
@@ -1,2 +1,56 @@
-package bridge;public class BridgePathBuilder {
+package bridge;
+
+import java.util.Collections;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
+public class BridgePathBuilder {
+    private final StringBuilder pathBuilder;
+    private final Map<PlayerChoice, Boolean> playerChoiceBooleanMap;
+
+    private BridgePathBuilder(Map<PlayerChoice, Boolean> playerMap){
+        pathBuilder = new StringBuilder();
+        playerChoiceBooleanMap = Collections.unmodifiableMap(playerMap);
+    }
+
+    public static BridgePathBuilder from(Map<PlayerChoice, Boolean> playerMap) {
+        return new BridgePathBuilder(playerMap);
+    }
+
+    public String buildSpecificPath(String playerChoiceValue) {
+        pathBuilder.setLength(0);
+
+        for (PlayerChoice playerChoice : playerChoiceBooleanMap.keySet()) {
+            Boolean rightOrWrong = playerChoiceBooleanMap.get(playerChoice);
+            if (playerChoice.hasSameValue(playerChoiceValue)) {
+                checkRightOrWrong(rightOrWrong);
+                continue;
+            }
+            appendWithSpace();
+        }
+
+        return joinSeparatorInPath(pathBuilder.toString());
+    }
+
+    private void appendWithSpace() {
+        pathBuilder.append(" ");
+    }
+
+    private String joinSeparatorInPath(String builtPath) {
+        List<String> convertedStringList = IntStream.range(0, builtPath.length())
+                .mapToObj(num -> String.valueOf(builtPath.charAt(num)))
+                .collect(Collectors.toList());
+
+        return String.join(" | ", convertedStringList);
+    }
+
+    private void checkRightOrWrong(Boolean rightOrWrong) {
+        if (rightOrWrong) {
+            pathBuilder.append("O");
+            return;
+        }
+        pathBuilder.append("X");
+    }
 }
--- src/test/java/bridge/BridgePathBuilderTest.java
+++ src/test/java/bridge/BridgePathBuilderTest.java
@@ -1,2 +1,37 @@
-package bridge;public class BridgePathBuilderTest {
+package bridge;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.util.LinkedHashMap;
+import java.util.Map;
+import org.junit.jupiter.api.Test;
+
+public class BridgePathBuilderTest {
+    @Test
+    void flw() {
+        Map<PlayerChoice, Boolean> map = new LinkedHashMap<>();
+        map.put(new PlayerChoice("U"), true);
+        map.put(new PlayerChoice("D"), true);
+        map.put(new PlayerChoice("U"), false);
+
+        BridgePathBuilder pathBuilder = BridgePathBuilder.from(map);
+        String expectedString = pathBuilder.buildSpecificPath("U");
+
+        assertThat(expectedString).isEqualTo("O |   | X");
+    }
+
+    @Test
+    void up_Down_Path_검증() {
+        Map<PlayerChoice, Boolean> map = new LinkedHashMap<>();
+        map.put(new PlayerChoice("U"), true);
+        map.put(new PlayerChoice("D"), true);
+        map.put(new PlayerChoice("U"), false);
+
+        BridgePathBuilder pathBuilder = BridgePathBuilder.from(map);
+        String expectedUpPath = pathBuilder.buildSpecificPath("U");
+        String expectedDownPath = pathBuilder.buildSpecificPath("D");
+
+        assertThat(expectedUpPath).isEqualTo("O |   | X");
+        assertThat(expectedDownPath).isEqualTo("  | O |  ");
+    }
 }
