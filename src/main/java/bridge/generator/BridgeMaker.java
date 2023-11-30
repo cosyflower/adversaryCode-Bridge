0cc3c8c5d4d8af2861e5289774cc1fa58ce2be54
sunghun <nokdonwithkj211@gmail.com>
2023-11-30 11:37:07 +0900

    feat(BridgeMaker): Bridge를 생성하는 필요한 값을 생성하는 클래스 생성

--- src/main/java/bridge/generator/BridgeMaker.java
+++ src/main/java/bridge/generator/BridgeMaker.java
@@ -1,2 +1,25 @@
-package bridge.generator;public class BridgeMaker {
+package bridge.generator;
+
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
+public class BridgeMaker {
+    private final BridgeNumberGenerator generator;
+    public BridgeMaker(BridgeNumberGenerator numberGenerator) {
+        generator = numberGenerator;
+    }
+
+    public List<String> makeBridge(int size) {
+        return IntStream.range(0, size)
+                .mapToObj(num -> convertNumberToString(generator.generate()))
+                .collect(Collectors.toList());
+    }
+
+    private String convertNumberToString(int randomNumber) {
+        if (randomNumber == 0) {
+            return "D";
+        }
+        return "U";
+    }
 }
