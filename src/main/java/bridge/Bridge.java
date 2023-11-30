6eeac201cb86ca8d65571af4cfbc509a6760888d
sunghun <nokdonwithkj211@gmail.com>
2023-11-30 09:10:35 +0900

    feat(Bridge): 생성된 다리를 관리하는 클래스 생성

--- src/main/java/bridge/Bridge.java
+++ src/main/java/bridge/Bridge.java
@@ -1,2 +1,27 @@
-package bridge;public class Bridge {
+package bridge;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Bridge {
+    private final List<String> bridgeValues;
+
+    public Bridge(List<String> bridgeValues) {
+        this.bridgeValues = new ArrayList<>(bridgeValues);
+    }
+
+    public boolean compareSpecificDigit(Digit digit, PlayerChoice playerChoice) {
+        if (hasSameValueAtIndex(digit, playerChoice)) {
+            return true;
+        }
+        return false;
+    }
+
+    private boolean hasSameValueAtIndex(Digit digit, PlayerChoice playerChoice) {
+        return bridgeValues.get(digit.getDigitValue()).equals(playerChoice.getPlayerChoiceValue());
+    }
+
+    public boolean hasSameLength(Digit digit) {
+        return bridgeValues.size() == digit.getDigitValue() - 1;
+    }
 }
