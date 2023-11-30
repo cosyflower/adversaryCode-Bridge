6dfe75265f78a6bf5ec35ffd403c6711cd586d0d
sunghun <nokdonwithkj211@gmail.com>
2023-11-30 09:12:13 +0900

    feat(TryTotal): 전체 시도 횟수를 관리하는 클래스 생성

--- src/main/java/bridge/TryTotal.java
+++ src/main/java/bridge/TryTotal.java
@@ -1,2 +1,17 @@
-package bridge;public class TryTotal {
+package bridge;
+
+public class TryTotal {
+    private final int tryTotalValue;
+
+    private TryTotal(int tryTotalValue) {
+        this.tryTotalValue = tryTotalValue;
+    }
+
+    public static TryTotal from(int defaultTryTotalValue) {
+        return new TryTotal(defaultTryTotalValue);
+    }
+
+    public TryTotal incrementRetryValue() {
+        return TryTotal.from(tryTotalValue + 1);
+    }
 }
