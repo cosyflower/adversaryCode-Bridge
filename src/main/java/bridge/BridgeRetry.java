e3b0dd63dc3a12fb79ceaa6a00f9d64312773efd
sunghun <nokdonwithkj211@gmail.com>
2023-11-30 09:11:36 +0900

    feat(BridgeRetry): 재실행 여부를 입력받은 값을 관리하는 클래스 생성

--- src/main/java/bridge/BridgeRetry.java
+++ src/main/java/bridge/BridgeRetry.java
@@ -1,2 +1,13 @@
-package bridge;public class BridgeRetry {
+package bridge;
+
+public class BridgeRetry {
+    private final String bridgeRetryValue;
+
+    public BridgeRetry(String bridgeRetryValue) {
+        this.bridgeRetryValue = bridgeRetryValue;
+    }
+
+    public boolean isRetryValue() {
+        return bridgeRetryValue.equals("R");
+    }
 }
