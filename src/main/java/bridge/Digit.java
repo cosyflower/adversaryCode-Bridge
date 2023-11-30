77f8d47a311a48ea44094e4885694bf99f005d68
sunghun <nokdonwithkj211@gmail.com>
2023-11-30 09:09:43 +0900

    feat(Digit): digit을 관리하는 클래스 작성

--- src/main/java/bridge/Digit.java
+++ src/main/java/bridge/Digit.java
@@ -1,2 +1,25 @@
-package bridge;public class Digit {
+package bridge;
+
+public class Digit {
+    private final int digitValue;
+
+    private Digit(int digit) {
+        this.digitValue = digit;
+    }
+
+    public static Digit from(int defaultValue) {
+        return new Digit(0);
+    }
+
+    public Digit increaseValue() {
+        return Digit.from(digitValue + 1);
+    }
+
+    public Digit clearValue() {
+        return Digit.from(0);
+    }
+
+    public int getDigitValue() {
+        return digitValue;
+    }
 }
